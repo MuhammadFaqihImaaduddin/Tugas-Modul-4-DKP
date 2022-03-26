b19868d683ed2e0fb24621dd62b0ee77cf92e83c
faizagst <faizagil.g@gmail.com>
2022-03-26 14:05:57 +0700

    Tugas Modul 4

---
+++ com/example/Modul4/Booster.java
@@ -0,0 +1,22 @@
+package com.example.Modul4;
+public class Booster {
+    public static void sinovac(){
+        
+    }
+    public static void astraZeneca(){
+        
+    }
+    public static void pfizer(){
+        
+    }
+    public static String moderna(){
+        return("Dosis Vaksin Booster adalah 1/2 Dosis Moderna");
+    }
+    public static String janssen(){
+        return("Dosis Vaksin Booster adalah 1/2 Dosis Moderna");
+    }
+    public static String sinopharm(String dosis){
+        return dosis;
+    }
+
+}
